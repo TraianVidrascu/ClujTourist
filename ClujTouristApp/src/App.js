65279fa02971e2b6688b855381e1c99f858cf508
Filip <phil_zir@yahoo.com>
2017-11-07 20:32:10 -0400

    Test

--- ClujTouristApp/src/App.js
+++ ClujTouristApp/src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import './App.css';
-
 import 'bootstrap/dist/css/bootstrap.css'
 import './assets/css/main.css'
 import { Route, BrowserRouter, Link, Redirect, Switch } from 'react-router-dom'
