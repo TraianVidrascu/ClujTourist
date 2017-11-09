fa31022429052512dd5e270de35cbb53ab556f4f
MadaMarginean <madalina_marginean96@yahoo.com>
2017-11-09 21:50:31 +0200

    Main menu

--- ClujTouristApp/src/App.js
+++ ClujTouristApp/src/App.js
@@ -1,23 +1,34 @@
 import React, { Component } from 'react';
+
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import lightBaseTheme from 'material-ui/styles/baseThemes/lightBaseTheme';
+
 import logo from './logo.svg';
 import './App.css';
 import Footer from "./components/footer/Footer";
+import Menu from './components/menu/Menu';7
+
+
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <MuiThemeProvider muiTheme={getMuiTheme(lightBaseTheme)}>
+        <div className="App">
+          <div className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h2>Welcome to React</h2>
+          </div>
+          <Menu />
+          <p className="App-intro">
+            To get started, edit <code>src/App.js</code> and save to reload.
+          </p>
           <div className="App-footer">
              <Footer/>
           </div>
-      </div>
+        </div>
+      </MuiThemeProvider>
     );
   }
 }
