e24f0a26fee488f475184e36815f343d5973bc73
user69 <adresadeemail.81@gmail.com>
2017-11-10 12:42:42 +0200

    header

--- ClujTouristApp/src/components/header/Header.js
+++ ClujTouristApp/src/components/header/Header.js
@@ -1,13 +1,7 @@
 import React, {Component} from 'react';
 import './Header.css';
-import IconMenu from 'material-ui/IconMenu';
-import IconButton from 'material-ui/IconButton';
 import FontIcon from 'material-ui/FontIcon';
-import NavigationExpandMoreIcon from 'material-ui/svg-icons/navigation/expand-more';
-import MenuItem from 'material-ui/MenuItem';
-import DropDownMenu from 'material-ui/DropDownMenu';
-import RaisedButton from 'material-ui/RaisedButton';
-import {Toolbar, ToolbarGroup, ToolbarSeparator, ToolbarTitle} from 'material-ui/Toolbar';
+import {Toolbar, ToolbarGroup} from 'material-ui/Toolbar';
 import Menu from '../menu/Menu';
 
 class Header extends Component {
@@ -15,12 +9,10 @@ class Header extends Component {
         return (
             <Toolbar>
                 <ToolbarGroup>
-                    <Menu>
-
-                    </Menu>
+                    <Menu/>
                 </ToolbarGroup>
                 <ToolbarGroup>
-                    <FontIcon>Cluj Tourist</FontIcon>
+                    <FontIcon>Welcome, (user)</FontIcon>
                 </ToolbarGroup>
             </Toolbar>
         )
