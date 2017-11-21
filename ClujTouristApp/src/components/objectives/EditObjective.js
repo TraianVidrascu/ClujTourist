9fd4a52763df2e7aacbd5e042ca26cc068266d78
Traian Vidrascu <traian.vidrascu@msg-systems.com>
2017-11-21 12:09:35 +0200

    [CRUD Events] edit event

--- ClujTouristApp/src/components/objectives/EditObjective.js
+++ ClujTouristApp/src/components/objectives/EditObjective.js
@@ -16,7 +16,9 @@ export default class EditObjective extends Component {
             address: '', // adresa
             tag_string: '',// utilizat pentru filtrare de exmplu #bar#nightlife#obiective sau #concerct#event
             note: 0, //nota de la 1 la 5, default 0
-            profile_image: ''// web link catre imagine
+            profile_image: '',// web link catre imagine
+            start_date: '',
+            end_date: '',
         }
         this.handleChange = this.handleChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
@@ -26,27 +28,31 @@ export default class EditObjective extends Component {
         this.state.key = this.props.match.params.id;
         this.state.itemRef.once('value')
             .then((snapshot) => {
-                let item = snapshot.val();
-                this.setState({
-                    item: item.id,
-                    id: item,
-                    name:
-                    item.name,
-                    description:
-                    item.description,
-                    location:
-                    item.location,
-                    address:
-                    item.address,
-                    tag_string:
-                    item.tag_string,
-                    note:
-                    item.note,
-                    profile_image:
-                    item.profile_image
-                })
-            }
-        )
+                    let item = snapshot.val();
+                    this.setState({
+                        item: item.id,
+                        id: item,
+                        name:
+                        item.name,
+                        description:
+                        item.description,
+                        location:
+                        item.location,
+                        address:
+                        item.address,
+                        tag_string:
+                        item.tag_string,
+                        note:
+                        item.note,
+                        profile_image:
+                        item.profile_image,
+                        start_date:
+                        item.start_date,
+                        end_date:
+                        item.end_date,
+                    })
+                }
+            )
     }
 
     handleChange(e) {
@@ -57,15 +63,29 @@ export default class EditObjective extends Component {
 
     handleSubmit(e) {
         e.preventDefault();
-        this.state.itemRef.update({
-            "name": this.state.name,
-            'description': this.state.description,
-            'location': this.state.location,
-            'address': this.state.address,
-            'tag_string': this.state.tag_string,
-            'note': this.state.note,
-            'profile_image': this.state.profile_image
-        });
+        if (this.state.start_date === undefined || this.state.end_date === undefined) {
+            this.state.itemRef.update({
+                "name": this.state.name,
+                'description': this.state.description,
+                'location': this.state.location,
+                'address': this.state.address,
+                'tag_string': this.state.tag_string,
+                'note': this.state.note,
+                'profile_image': this.state.profile_image
+            });
+        }else{
+            this.state.itemRef.update({
+                "name": this.state.name,
+                'description': this.state.description,
+                'location': this.state.location,
+                'address': this.state.address,
+                'tag_string': this.state.tag_string,
+                'note': this.state.note,
+                'profile_image': this.state.profile_image,
+                'start_date': this.state.start_date,
+                'end_date': this.state.end_date,
+            });
+        }
         NotificationManager.success('Update completed', 'Success');
 
     }
@@ -73,31 +93,49 @@ export default class EditObjective extends Component {
     render() {
         return (<div className="row">
             <NotificationContainer className="alert alert-success"/>
-            <h1>Edit { this.state.name}</h1>
-             <form onSubmit={this.handleSubmit}>
+            <h1>Edit {this.state.name}</h1>
+            <form onSubmit={this.handleSubmit}>
                 <div className="form-group">
                     <label>Name</label>
-                    <input type="text" className="form-control" name="name" placeholder="Name" onChange={this.handleChange} value={this.state.name} />
+                    <input type="text" className="form-control" name="name" placeholder="Name"
+                           onChange={this.handleChange} value={this.state.name}/>
                 </div>
                 <div className="form-group">
-                    <label >Image</label>
-                    <input type="text" className="form-control" name="profile_image" placeholder="Profile Image" onChange={this.handleChange} value={this.state.profile_image}/>
+                    <label>Image</label>
+                    <input type="text" className="form-control" name="profile_image" placeholder="Profile Image"
+                           onChange={this.handleChange} value={this.state.profile_image}/>
                 </div>
                 <div className="form-group">
-                    <label >Description</label>
-                    <textarea className="form-control" rows="5" name="description" value={this.state.description} onChange={this.handleChange} />
+                    <label>Description</label>
+                    <textarea className="form-control" rows="5" name="description" value={this.state.description}
+                              onChange={this.handleChange}/>
                 </div>
                 <div className="form-group">
-                    <label >Location</label>
-                    <textarea className="form-control" name="location" value={this.state.location} onChange={this.handleChange} />
+                    <label>Location</label>
+                    <textarea className="form-control" name="location" value={this.state.location}
+                              onChange={this.handleChange}/>
                 </div>
                 <div className="form-group">
-                    <label >Address</label>
-                    <textarea className="form-control" name="address" value={this.state.address} onChange={this.handleChange} />
+                    <label>Address</label>
+                    <textarea className="form-control" name="address" value={this.state.address}
+                              onChange={this.handleChange}/>
                 </div>
                 <div className="form-group">
-                    <label > Tags</label>
-                    <textarea className="form-control" name="tag_string" value={this.state.tag_string} onChange={this.handleChange} />
+                    <label> Tags</label>
+                    <textarea className="form-control" name="tag_string" value={this.state.tag_string}
+                              onChange={this.handleChange}/>
+                </div>
+                <div className="form-group"
+                     style={{visibility: this.state.start_date !== undefined ? 'visible' : 'hidden'}}>
+                    <label>Start Date</label>
+                    <input type="datetime-local" className="form-control" name="start_date"
+                           value={this.state.start_date} onChange={this.handleChange}/>
+                </div>
+                <div className="form-group"
+                     style={{visibility: this.state.end_date !== undefined ? 'visible' : 'hidden'}}>
+                    <label>End Date</label>
+                    <input type="datetime-local" className="form-control" name="end_date" value={this.state.end_date}
+                           onChange={this.handleChange}/>
                 </div>
                 <button type="submit" className="btn btn-primary">Save changes</button>
             </form>
