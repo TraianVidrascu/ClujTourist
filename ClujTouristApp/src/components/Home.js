f89a2c67274c7ee04a61ff2869aa1a2af26ab011
Bedeoan Alexandru <axi.bedeoan@gmail.com>
2017-12-12 17:10:05 +0200

    Add filters: all, events, locations

--- ClujTouristApp/src/components/Home.js
+++ ClujTouristApp/src/components/Home.js
@@ -11,240 +11,149 @@ export default class Home extends Component {
         super();
         this.state = {
             items: [],
-            searchKey: "",
-            shouldHide: false,
-            items_filtered: []
+            search: '',
+            filter: 'all',
         }
-        this.handleChange = this.handleChange.bind(this)
     }
-    async handleChange (e) {
-        this.setState({
-            searchKey: e.target.value
-        });
-        if(this.state.searchKey.length < 2){
-            this.setState({
-                items_filtered: this.state.items
-            });
-        }
-        else if(this.state.searchKey.length > 1) {
-            this.setState({
-                shouldHide: true
-            });
-            let key = this.state.searchKey
-            const itemsRef = firebase.database().ref('items');
-            itemsRef.on('value', (snapshot) => {
-                let items = snapshot.val();
-                let newState = [];
-                for (let item in items) {
-                    if(items[item].name.toLowerCase().includes(key.toLowerCase())){
-                        newState.push({
-                            id: item,
-                            name: items[item].name,
-                            description: items[item].description,
-                            location: items[item].location,
-                            address: items[item].address,
-                            tag_string: items[item].tag_string,
-                            note: items[item].note,
-                            profile_image: items[item].profile_image
-                        });
-                    }
-                }
-                this.setState({
-                    items_filtered: newState
-                });
-            });
-        }
-     }
     componentDidMount() {
-        const itemsRef = firebase.database().ref('items');
+        const itemsRef = firebase.database().ref('items')
+
         itemsRef.on('value', (snapshot) => {
-            let items = snapshot.val();
-            let newState = [];
-            for (let item in items) {
-                newState.push({
-                    id: item,
-                    name: items[item].name,
-                    description: items[item].description,
-                    location: items[item].location,
-                    address: items[item].address,
-                    tag_string: items[item].tag_string,
-                    note: items[item].note,
-                    profile_image: items[item].profile_image
-                });
-            }
+            const items = snapshot.val()
             this.setState({
-                items: newState
-            });
+                items: Object.keys(items).map(id => ({ id, ...items[id] }))
+            })
         });
     }
+    changeSearch(value) {
+        this.setState({
+            search: value
+        })
+    }
+    changeFilter(value) {
+        this.setState({
+            filter: value
+        })
+    }
 
-    deleteRow = (item,e) => {
-        e.preventDefault();
+    deleteRow(id) {
+        const newItems = this.state.items.filter(item => item.id !== id)
 
-        var items = this.state.items.filter(function(itm) {
-            return item !== itm.id;
-        });
-        this.setState({ items: items });
+        this.setState({ items: newItems });
 
-        firebase.database().ref('items').child(item);
-    };
-    formPreventDefault(e) {
-        e.preventDefault();
+        // stergerea nu se face pe firebase
+        // firebase.database().ref('items').child(item);
+    }
+    filterItems() {
+        const { search, filter, items } = this.state
+
+        return items
+            .filter(item => {
+                if (search === '') return true
+                if (search.length < 2) return false
+                return item.name.toLowerCase().includes(search.toLowerCase())
+            })
+            .filter(item => {
+                if (filter === 'all') return true
+                if (filter === 'events') return item.start_date !== undefined
+                if (filter === 'locations') return item.start_date === undefined
+            })
+    }
+    renderSearch() {
+        const { search } = this.state
+
+        return (
+            <input
+                type="text"
+                value={search}
+                onChange={e => this.changeSearch(e.target.value)}
+                className="form-control"
+                style={{ marginBottom: 20 }}
+                placeholder="Search by title..."
+            />
+        )
+    }
+    renderFilter() {
+        const { filter } = this.state
+
+        return (
+            <div className="btn-group" style={{ marginBottom: 20 }}>
+                <button
+                    className={'btn btn-default' + (filter === 'all' ? ' active' : '')}
+                    onClick={() => this.changeFilter('all')}
+                >
+                    All
+                </button>
+                <button
+                    className={'btn btn-default' + (filter === 'events' ? ' active' : '')}
+                    onClick={() => this.changeFilter('events')}
+                >
+                    Events
+                </button>
+                <button
+                    className={'btn btn-default' + (filter === 'locations' ? ' active' : '')}
+                    onClick={() => this.changeFilter('locations')}
+                >
+                    Locations
+                </button>
+            </div>
+        )
+    }
+    renderItems() {
+        const items = this.filterItems()
+        const { authed } = this.props
+
+        return (
+            <div className="row justify-content-md-center">
+                {items.map(item => (
+                    <div key={item.id} className="col-xs-12 col-sm-6 col-md-6">
+                        <div className="well well-sm">
+                            <div className="row">
+                                <div className="col-sm-6 col-md-4">
+                                    <img src={item.profile_image} alt="" className="img-rounded img-responsive" />
+                                </div>
+                                <div className="col-sm-6 col-md-8">
+                                    <h4>{item.name}</h4>
+                                    <p>{item.description.slice(0, 95)} ... </p>
+                                    {authed === false ? (
+                                        <div className="btn-group">
+                                            <Link to={'/objectives/' + item.id}>
+                                                <button className="btn btn-primary">
+                                                    <span className="glyphicon glyphicon-info-sign" /> Info
+                                                    </button>
+                                            </Link>
+                                        </div>
+                                    ) : (
+                                        <div className="btn-group admin-buttons">
+                                            <button className="btn btn-primary"
+                                                onClick={() => this.deleteRow(item.id)}>
+                                                <span className="glyphicon glyphicon-remove" />
+                                            </button>
+                                            <button className="btn btn-primary">
+                                                <Link to={'/objectives/' + item.id}>
+                                                    <span className="glyphicon glyphicon-edit" />
+                                                </Link>
+                                            </button>
+                                            <button className="btn btn-primary">
+                                                <span className="glyphicon glyphicon-info-sign" />
+                                            </button>
+                                        </div>
+                                    )}
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                ))}
+            </div>
+        )
     }
     render() {
-        if (this.props.authed === false) {
-            return (
-                <div className="container">
-                    <div className="row home_search">
-                        <div className="form-group">
-                            <form onSubmit={this.formPreventDefault} >
-                                <input type="text" onChange={this.handleChange} className="form-control" id="keyword" placeholder="Search by title.."/>
-                            </form>
-                        </div>
-                    </div><br/>
-                    <div className="row justify-content-md-center">
-                        <div className="list-group" className={!this.state.shouldHide ? 'hidden' : ''}>
-                            {this.state.items_filtered.map((item, index) => {
-                                return (
-                                    <div key={index} className="col-xs-12 col-sm-6 col-md-6">
-                                        <div className="well well-sm">
-                                            <div className="row">
-                                                <div className="col-sm-6 col-md-4">
-                                                    <img src={item.profile_image} alt="" className="img-rounded img-responsive" />
-                                                </div>
-                                                <div className="col-sm-6 col-md-8">
-                                                    <h4>{item.name}</h4>
-                                                    <p>{item.description.slice(0,95)} .. </p>
-                                                    <div className="btn-group">
-                                                        <Link to={'/objectives/' + item.id}>
-                                                            <button type="submit" className="btn btn-primary">
-                                                                <span className="glyphicon glyphicon-info-sign"/> Info
-                                                            </button>
-                                                        </Link>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                )
-                            })}
-                        </div>
-                        <div className="list-group" className={this.state.shouldHide ? 'hidden' : ''}>
-                            {this.state.items.map((item, index) => {
-                                return (
-                                    <div key={index} className="col-xs-12 col-sm-6 col-md-6">
-                                        <div className="well well-sm">
-                                            <div className="row">
-                                                <div className="col-sm-6 col-md-4">
-                                                    <img src={item.profile_image} alt="" className="img-rounded img-responsive" />
-                                                </div>
-                                                <div className="col-sm-6 col-md-8">
-                                                    <h4>{item.name}</h4>
-                                                    <p>{item.description.slice(0,95)} .. </p>
-                                                    <div className="btn-group">
-                                                        <Link to={'/objectives/' + item.id}>
-                                                            <button type="submit" className="btn btn-primary">
-                                                                <span className="glyphicon glyphicon-info-sign"/> Info
-                                                            </button>
-                                                        </Link>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                )
-                            })}
-                        </div>
-                    </div>
-                </div>
-            );
-        } else {
-            return (
-                <div className="container">
-                    <div className="row home_search">
-                        <div className="form-group">
-                            <form onSubmit={this.formPreventDefault} >
-                                <input type="text" onChange={this.handleChange} className="form-control" id="keyword" placeholder="Search by title.."/>
-                            </form>
-                        </div>
-                    </div><br/>
-                    <div className="row justify-content-md-center">
-                        <div className="list-group" className={!this.state.shouldHide ? 'hidden' : ''}>
-                            {this.state.items_filtered.map((item, index) => {
-                                return (
-                                    <div key={index} className="col-xs-12 col-sm-6 col-md-6">
-                                        <div className="well well-sm">
-                                            <div className="row">
-                                                <div className="col-sm-6 col-md-4">
-                                                    <img src={item.profile_image} alt="" className="img-rounded img-responsive" />
-                                                </div>
-                                                <div className="col-sm-6 col-md-8">
-                                                    <h4>{item.name}</h4>
-                                                    <p>{item.description.slice(0,95)} .. </p>
-                                                    <div className="btn-group admin-buttons">
-
-                                                        <button className="btn btn-primary" type="submit"
-                                                                onClick={(e) => this.deleteRow(item.id, e)}>
-                                                            <span className="glyphicon glyphicon-remove"/>
-                                                        </button>
-                                                        <button type="submit" className="btn btn-primary">
-
-                                                            <Link to={'/objectives/' + item.id}>
-                                                                <span className="glyphicon glyphicon-edit"/>
-                                                            </Link>
-
-                                                        </button>
-                                                        <button className="btn btn-primary" type="submit">
-                                                            <span className="glyphicon glyphicon-info-sign"/>
-                                                        </button>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                )
-                            })}
-                        </div>
-                        <div className="list-group" className={this.state.shouldHide ? 'hidden' : ''}>
-                            {this.state.items.map((item, index) => {
-                                return (
-
-                                    <div key={index} className="col-xs-12 col-sm-6 col-md-6">
-                                        <div className="well well-sm">
-                                            <div className="row">
-                                                <div className="col-sm-6 col-md-4">
-                                                    <img src={item.profile_image} alt="" className="img-rounded img-responsive" />
-                                                </div>
-                                                <div className="col-sm-6 col-md-8">
-                                                    <h4>{item.name}</h4>
-                                                    <p>{item.description.slice(0,95)} .. </p>
-                                                    <div className="btn-group admin-buttons">
-
-                                                        <button className="btn btn-primary" type="submit"
-                                                                onClick={(e) => this.deleteRow(item.id, e)}>
-                                                            <span className="glyphicon glyphicon-remove"/>
-                                                        </button>
-                                                        <button type="submit" className="btn btn-primary">
-
-                                                            <Link to={'/objectives/' + item.id}>
-                                                                <span className="glyphicon glyphicon-edit"/>
-                                                            </Link>
-
-                                                        </button>
-                                                        <button className="btn btn-primary" type="submit">
-                                                            <span className="glyphicon glyphicon-info-sign"/>
-                                                        </button>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                )
-                            })}
-                        </div>
-                    </div>
-                </div>
-            );
-        }
+        return (
+            <div className="container">
+                {this.renderSearch()}
+                {this.renderFilter()}
+                {this.renderItems()}
+            </div>
+        )
     }
 }
